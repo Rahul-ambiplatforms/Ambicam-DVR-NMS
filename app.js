dfa16b2a6a06a7986216091c4d29714f97b8b2c1
RahulVadhiya <rahul@ambiplatforms.com>
2023-12-19 16:12:45 +0530

    Module Changed
    Video Bitrate Counting

--- app.js
+++ app.js
@@ -30,12 +30,25 @@ nms.on('postPublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
   const streamId = StreamPath.split("/")[1];
   const epochTime = moment().format("DD_MM_YY");
+
+    // Create a folder with today's date inside the StreamPath if it doesn't exist
+  const todayFolderPath = path.join(__dirname, 'Recordings', StreamPath, epochTime);
+  if (!fs.existsSync(todayFolderPath)) {
+    fs.mkdirSync(todayFolderPath, { recursive: true });
+  }
+
+   // Create the initial m3u8 file with basic header information
+   const m3u8Content = ``;
+   const m3u8FilePath = path.join(todayFolderPath, 'index.m3u8');
+   fs.writeFileSync(m3u8FilePath, m3u8Content);
+
+   
   const taskConfig = {
     app: streamId,
     ac: "aac",
     hls: true,
     hlsKeep: true,
-    hlsFlags:  `[hls_time=30:hls_list_size=5000:hls_flags=append_list:strftime=1:hls_segment_filename=./Recordings${StreamPath}/${epochTime}/%s.ts]`,
+    hlsFlags:  `[hls_time=6:hls_list_size=5000:hls_flags=append_list:strftime=1:hls_segment_filename=./Recordings${StreamPath}/${epochTime}/%s.ts]`,
   };
   // Append the task configuration to the trans tasks
   config.trans.tasks.push(taskConfig);
